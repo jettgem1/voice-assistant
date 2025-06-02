820a93c475712f0cc15a1969148a54fc91ba20e6
jmg575 <jettgem1@gmail.com>
2025-06-02 16:47:21 -0700

    Another time

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: process.env.NODE_ENV === 'production' ? '/automate' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/automate' : '',
-  reactStrictMode: true,
-  swcMinify: true,
-}
-
-module.exports = nextConfig 
