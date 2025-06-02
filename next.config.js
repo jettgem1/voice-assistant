56a5d82c4710a0cb133f78a6ec7bd7f1a11a080d
jmg575 <jettgem1@gmail.com>
2025-06-02 16:34:14 -0700

    Trying to bug fix 404 error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/automate',
+  basePath: process.env.NODE_ENV === 'production' ? '/automate' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/automate' : '',
   reactStrictMode: true,
   swcMinify: true,
 }
